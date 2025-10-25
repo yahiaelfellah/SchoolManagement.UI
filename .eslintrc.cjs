0c0b387689c24ec7b446af0ad8c61f8a9e7e47d5
yahia <yahia.elfellah@molinerocapital.com>
2025-10-25 13:15:53 -0400

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,34 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/eslint-config-typescript",
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [["@", "./src"]],
+        extensions: [".js", ".ts", ".vue"],
+      },
+    },
+  },
+  rules: {
+    "vue/multi-word-component-names": "off", // optional
+    "vue/no-mutating-props": "off",
+    "import/no-unresolved": "off", // optional: silence path errors if resolver fails
+  },
+};
