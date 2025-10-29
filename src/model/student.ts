658a6fd8df039ad6e885e8d08e04bc90a3bbaa64
yahia <yahia.elfellah@molinerocapital.com>
2025-10-29 13:42:20 -0400

    update models :  - create new models for students and class and updloadFile

---
+++ src/model/student.ts
@@ -0,0 +1,9 @@
+// Interfaces
+export default interface Student {
+  id: number
+  firstName: string
+  lastName: string
+  dateofBirth?: string
+  gender?: string
+  address?: string
+}
---
+++ src/model/uploadFile.ts
@@ -0,0 +1,6 @@
+export default interface UploadFile {
+  uid: string
+  name: string
+  url?: string
+  originFileObj?: File
+}
