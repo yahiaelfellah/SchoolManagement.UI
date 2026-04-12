7046b9c508d9f9d9cfe49e1890e63c707427b1ae
yahia <yahia.elfellah@molinerocapital.com>
2026-04-12 09:25:02 -0400

    fix(electron): Correctly import autoUpdater from electron-updater package

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,6 @@
 import { app, BrowserWindow, dialog } from 'electron'
-import { autoUpdater } from 'electron-updater'
+import pkg from 'electron-updater';
+const { autoUpdater } = pkg;
 import path from 'path'
 import { fileURLToPath } from 'url'
 import net from 'net'
